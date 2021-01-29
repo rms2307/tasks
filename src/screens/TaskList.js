f804f142bb7b99347cff825a063fbdb260feed99
Robson Silva <robson.silva@tripletech.com.br>
2021-01-29 06:56:36 -0300

    componente Task

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -7,6 +7,8 @@ import todayImage from '../../assets/imgs/today.jpg'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 
+import Task from '../components/Task'
+
 export default class TaskList extends Component {
     render() {
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
@@ -20,7 +22,42 @@ export default class TaskList extends Component {
                     </View>
                 </ImageBackground>
                 <View style={styles.taskList}>
-                    <Text>TaskList</Text>
+                    <Task descricao='Comprar Livro' estimateAt={new Date()}
+                    doneAt={new Date()} />
+                    <Task descricao='Ler Livro' estimateAt={new Date()}
+                    doneAt={null} />
+                    <Task descricao='Comprar Livro' estimateAt={new Date()}
+                    doneAt={new Date()} />
+                    <Task descricao='Ler Livro' estimateAt={new Date()}
+                    doneAt={null} />
+                    <Task descricao='Comprar Livro' estimateAt={new Date()}
+                    doneAt={new Date()} />
+                    <Task descricao='Ler Livro' estimateAt={new Date()}
+                    doneAt={null} />
+                    <Task descricao='Comprar Livro' estimateAt={new Date()}
+                    doneAt={new Date()} />
+                    <Task descricao='Ler Livro' estimateAt={new Date()}
+                    doneAt={null} />
+                    <Task descricao='Comprar Livro' estimateAt={new Date()}
+                    doneAt={new Date()} />
+                    <Task descricao='Ler Livro' estimateAt={new Date()}
+                    doneAt={null} />
+                    <Task descricao='Comprar Livro' estimateAt={new Date()}
+                    doneAt={new Date()} />
+                    <Task descricao='Ler Livro' estimateAt={new Date()}
+                    doneAt={null} />
+                    <Task descricao='Comprar Livro' estimateAt={new Date()}
+                    doneAt={new Date()} />
+                    <Task descricao='Ler Livro' estimateAt={new Date()}
+                    doneAt={null} />
+                    <Task descricao='Comprar Livro' estimateAt={new Date()}
+                    doneAt={new Date()} />
+                    <Task descricao='Ler Livro' estimateAt={new Date()}
+                    doneAt={null} />
+                    <Task descricao='Comprar Livro' estimateAt={new Date()}
+                    doneAt={new Date()} />
+                    <Task descricao='Ler Livro' estimateAt={new Date()}
+                    doneAt={null} />
                 </View>
             </View>
         )
